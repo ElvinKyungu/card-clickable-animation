c1237a8eafdb78c50fb027d660f4bba5dc33c983
elvinkyungu <elvinkyungu.75@gmail.com>
2024-06-18 17:32:25 +0200

    init card clickable style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
 <script setup lang="ts">
+  import Card from './components/Card.vue'
 </script>
 
 <template>
-  <RouterView />
+  <main class="bg-gray-900 flex items-center justify-center h-screen w-full">
+    <section class="mx-[35rem] py-5 relative w-1/2 shadow rounded-xl bg-gray-800">
+      <Card
+        image="https://animations.dev/how-i-use-framer-motion/how-i-code-animations/pirate.png"
+        title="Pirates in the jungle"
+        subtitle="Find the treasure."
+        paragraphe="You are a pirate and you have to find the treasure in the jungle. But be careful, there are traps and wild animals. Some filler text to make it longer. Maybe even longer, because it looks better."
+      />
+      <Card
+        image="https://animations.dev/how-i-use-framer-motion/how-i-code-animations/boy.webp"
+        title="Lost in the mountains"
+        subtitle="Be careful."
+        paragraphe="You are lost in the mountains and you have to find your way home. But be careful, there are dangerous animals and you can get lost."
+      />
+      <Card
+        image="https://animations.dev/how-i-use-framer-motion/how-i-code-animations/pirate.png"
+        title="Pirates in the jungle"
+        subtitle="Find the treasure."
+        paragraphe="You are a pirate and you have to find the treasure in the jungle. But be careful, there are traps and wild animals. Some filler text to make it longer. Maybe even longer, because it looks better."
+      />
+      <Card
+        image="https://animations.dev/how-i-use-framer-motion/how-i-code-animations/boy.webp"
+        title="Lost in the mountains"
+        subtitle="Be careful."
+        paragraphe="You are lost in the mountains and you have to find your way home. But be careful, there are dangerous animals and you can get lost."
+      />
+    </section>
+  </main>
 </template>
 
 <style scoped>
---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import gsap from 'gsap';
+import { ref } from 'vue';
+const props = defineProps({
+  image: String,
+  title: String,
+  subtitle: String,
+  paragraphe: String
+})
+const showParagraph = ref(false);
+
+const handleCardClick = (event: Event) => {
+  const cardElement = event.currentTarget as HTMLElement;
+  showParagraph.value = !showParagraph.value;
+
+};
+</script>
+<template>
+  <article
+    @click="handleCardClick"
+    class="
+      px-5 py-2 bg-gray-800 cursor-pointer transition-colors text-white 
+      rounded-xl mx-24
+    "
+  >
+    <div class="">
+      <div class="flex gap-4 justify-between w-full bg-gray-800">
+        <img :src="image" class="w-20 bg-gray-800" alt="">
+        <div class="border-b border-gray-600 flex justify-between items-center w-full">
+          <div>
+            <h3>{{ title }}</h3>
+            <span>{{ subtitle }}</span>
+          </div>
+          <button>Get</button>
+        </div>
+      </div>
+      <p class="my-2 transition-all delay-100" v-if="showParagraph">{{ paragraphe }}</p>
+    </div>
+  </article>
+</template>
